f9fb3dbb3a63d998e7f417359bf6697b6b1fb783
omarshokry11 <64105333+omarshokry11@users.noreply.github.com>
2020-07-25 03:07:46 +0200

    init

---
+++ src/Layout/components/MainNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const MainNav = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+
+export default MainNav;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App.js';
+
+// global css
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css";
+import "./Layout/components/style/Main.scss";
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </div>,
+  document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
